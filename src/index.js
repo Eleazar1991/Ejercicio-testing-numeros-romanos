907c5ffae449d8f31074c4c6db1046e419655a3f
Eleazar Pérez Arencibia <eleazar.yio@gmail.com>
2018-04-20 20:36:56 +0100

    Faltan los ifs para contemplar todos los numeros romanos

--- src/index.js
+++ src/index.js
@@ -6,14 +6,60 @@
 
 // module.exports=fizzbuzz;
 
-export const fizzbuzz = (value) => {
-    if (!(value % 3) && !(value % 5)) { //value==6
-        return 'FIZZBUZZ'
-    } else if (!(value % 5)) { //value==6
-        return 'BUZZ'
-    } else if (!(value % 3)) {
-        return 'FIZZ'
-    } else {
-        return "" + value;
+export const romanNumbers = (value) => {
+    var map = {
+        1000:"M",
+        500:"D",
+        100:"C",
+        50:"L",
+        10:"X",
+        9:"IX",
+        5:"V",
+        4:"IV",
+        1:"I"
     }
+    var romanNumber = ""
+    return convertToRomanNumber(map,value,romanNumber)
+}
+
+function convertToRomanNumber(map,value,romanNumber){
+    if(value>=1000){
+        romanNumber+=map['1000']
+        value-=1000;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=500){
+        romanNumber+=map['500']
+        value-=500;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=100){
+        romanNumber+=map['100']
+        value-=100;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=50){
+        romanNumber+=map['50']
+        value-=50;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=10){
+        romanNumber+=map['10']
+        value-=10;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=5){
+        romanNumber+=map['5']
+        value-=5;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value>=1){
+        romanNumber+=map['1']
+        value-=1;
+        return convertToRomanNumber(map,value,romanNumber);
+    }
+    if(value==0){
+        return romanNumber;
+    }
+    
 }
--- test/index.test.js
+++ test/index.test.js
@@ -1,31 +1,15 @@
-//test fizzbuzz
-// const fizzbuzz = require('../src/fizzbuzz');
-import { fizzbuzz } from "../src";
-/* 
-describe('Index', () => {
-    test('Debe sumar dos numeros', () => {
-        expect(2 + 3).toBe(5);
-    })
-}) */
-describe('Index', () => {
-    test('Debería devolver un number como un string', () => {
-        expect(fizzbuzz(1)).toBe('1');
-    })
-    test('Deberia return FIZZ si number es divisible por 3', () => {
-        expect(fizzbuzz(6)).toBe('FIZZ');
-    })
-    test('Debería retrun FIZZ si number es divisible por 3', () => {
-        expect(fizzbuzz(6)).toBe('FIZZ');
-        expect(fizzbuzz(18)).toBe('FIZZ');
-        expect(fizzbuzz(21)).toBe('FIZZ');
-    })
-    test('Debería retrun BUZZ si number es divisible por 5', () => {
-        expect(fizzbuzz(5)).toBe('BUZZ');
-        expect(fizzbuzz(10)).toBe('BUZZ');
-        expect(fizzbuzz(25)).toBe('BUZZ');
-    })
-    test('Debería retrun FIZZBUZZ si number es divisible por 5 y por 3', () => {
-        expect(fizzbuzz(15)).toBe('FIZZBUZZ');
+import { romanNumbers } from "../src";
+
+describe('roman numbers should', () => {
+    test('return I if number is one', () => {
+        expect(romanNumbers(1)).toBe('I');
     })
 
+    test('return XVIII if number is 18', () => {
+        expect(romanNumbers(18)).toBe('XVIII');
+    })
+    
+    test('return XIX if number is 19', () => {
+        expect(romanNumbers(19)).toBe('XIX');
+    })
 })
